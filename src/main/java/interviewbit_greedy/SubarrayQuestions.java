9e49240107c650a4b4cd76382d5a0c0ee95fb0e3
Kevin Mawhorter <kdmawhorter@gmail.com>
2022-04-13 16:15:33 -0400

    SUBARRAY: Added max product implementation.

--- src/main/java/interviewbit_greedy/SubarrayQuestions.java
+++ src/main/java/interviewbit_greedy/SubarrayQuestions.java
@@ -25,6 +25,39 @@ public class SubarrayQuestions {
             return longestArray[0];
         }
         return 0;
+    }
 
+    /**
+     * Returns the maximum product of any contiguous subarray of an input array.
+     *
+     * @param inputArray - an int array
+     * @return the maximum contiguous subarray product
+     */
+    public int findMaxProduct(int[] inputArray) {
+        if (inputArray!=null && inputArray.length>0) {
+            int[] largestPositiveArray = new int[inputArray.length];
+            int[] largestNegativeArray = new int[inputArray.length];
+
+            int maxProduct = 0;
+
+            for (int i = largestNegativeArray.length - 1; i >= 0; i--) {
+                largestNegativeArray[i] = inputArray[i];
+                largestPositiveArray[i] = inputArray[i];
+
+                if (i+1 < largestNegativeArray.length) {
+                    largestPositiveArray[i] = Math.max(Math.max(largestPositiveArray[i],
+                                                                largestPositiveArray[i]*largestPositiveArray[i+1]),
+                                                                largestPositiveArray[i]*largestNegativeArray[i+1]);
+                    largestNegativeArray[i] = Math.min(Math.max(largestNegativeArray[i],
+                                                                largestNegativeArray[i]*largestPositiveArray[i+1]),
+                                                                largestNegativeArray[i]*largestNegativeArray[i+1]);
+                }
+
+                maxProduct = Math.max(maxProduct, largestPositiveArray[i]);
+            }
+            return maxProduct;
+
+        }
+        return 0;
     }
 }
--- src/test/java/interviewbit_greedy/SubarrayQuestionsTest.java
+++ src/test/java/interviewbit_greedy/SubarrayQuestionsTest.java
@@ -13,6 +13,9 @@ public class SubarrayQuestionsTest {
     private final int[] LONG_SEQ_TEST_2_EMPTY = {};
     private final int[] LONG_SEQ_TEST_3_ONE_ELM = {1};
     private final int[] LONG_SEQ_TEST_4_INTERLEAVE = {1, 1, 13, 2, 4, 5, 16, 18, 11};
+    private final int[] LONG_SEQ_TEST_5_NEGATIVE = {3, 6, 1, 4, -2, 8};
+    private final int[] LONG_SEQ_TEST_6_DBL_NEG = {3, 6, 1, 4, -2, 8, -4};
+    private final int[] LONG_SEQ_TEST_7_ZERO = {0, 3, 6, -1, 4, 0, 8, -4};
 
     @BeforeEach
     void init() {
@@ -29,6 +32,23 @@ public class SubarrayQuestionsTest {
                 "Test 3 - One Element");
         assertEquals(7, subarrayQuestions.longestSubsequenceDetermination(LONG_SEQ_TEST_4_INTERLEAVE),
                 "Test 4 - Interleaved");
-
+    }
+    
+    @Test
+    void findMaxProductTest() {
+        assertEquals(252, subarrayQuestions.findMaxProduct(LONG_SEQ_TEST_1_BASE),
+                "Test 1 - Base");
+        assertEquals(0, subarrayQuestions.findMaxProduct(LONG_SEQ_TEST_2_EMPTY), 
+                "Test 2 - Empty");
+        assertEquals(1, subarrayQuestions.findMaxProduct(LONG_SEQ_TEST_3_ONE_ELM), 
+                "Test 3 - One Element");
+        assertEquals(1_647_360, subarrayQuestions.findMaxProduct(LONG_SEQ_TEST_4_INTERLEAVE), 
+                "Test 4 - Interleaved");
+        assertEquals(72, subarrayQuestions.findMaxProduct(LONG_SEQ_TEST_5_NEGATIVE), 
+                "Test 5 - Negative");
+        assertEquals(4608, subarrayQuestions.findMaxProduct(LONG_SEQ_TEST_6_DBL_NEG), 
+                "Test 6 - Double Negative");
+        assertEquals(18, subarrayQuestions.findMaxProduct(LONG_SEQ_TEST_7_ZERO), 
+                "Test 7 - Zeros");
     }
 }
