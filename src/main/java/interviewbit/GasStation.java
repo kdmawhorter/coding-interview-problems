c1cc9043e599517535ff54701c874bedb6e52ae4
Kevin Mawhorter <kdmawhorter@gmail.com>
2022-04-26 15:22:07 -0400

    COMMENTS: Gas Station

--- src/main/java/interviewbit/GasStation.java
+++ src/main/java/interviewbit/GasStation.java
@@ -1,16 +1,20 @@
 package interviewbit;
 
+/**
+ * A class designed to answer problems related to InterviewBit Amazon Problems
+ * <a href="https://www.interviewbit.com/problems/gas-station/">Edit Distance</a>.
+ */
 public class GasStation {
 
     /**
      * Given an array of fuel replenishments and fuel costs, calculates the smallest starting index where the arrays
      * can be traversed in a cycle. At an index i, the index can be advanced if the fuel accumulated for the traversal
-     * plus the fuel replenished at index i less the fuel cost at i. The only allowed motion is left-to-right cycle for
-     * the arrays (i.e. 0->1->2->...->(n-2)->(n-1)->0->1.
+     * plus the fuel replenished at index i is less the fuel cost at i. The only allowed motion is a left-to-right cycle
+     * for the array (i.e. 0->1->2->...->(n-2)->(n-1)->0->1).
      *
-     * @param fuelReplenishment - an int array dictating how much fuel is restored at each index
-     * @param fuelCostToMove - an int array dictating how much fuel is spent to advance from that index
-     * @return int - the smallest index from which a cycle may be completed, or -1 if none.
+     * @param fuelReplenishment an array dictating how much fuel is restored at each index
+     * @param fuelCostToMove an array dictating how much fuel is spent to advance from that index
+     * @return the smallest index from which a cycle may be completed, or -1 if none.
      */
 
     public int minimumCycleIndex(int[] fuelReplenishment, int[] fuelCostToMove) {
