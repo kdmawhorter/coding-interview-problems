f17a2216c2f97f539d737d083c2e4dfdca8f902b
Kevin Mawhorter <kdmawhorter@gmail.com>
2022-04-26 13:39:16 -0400

    COMMENTS: Buying Stocks

--- src/main/java/interviewbit/BuyingStocks.java
+++ src/main/java/interviewbit/BuyingStocks.java
@@ -1,11 +1,17 @@
 package interviewbit;
 
+/**
+ * A class designed to answer problems related to InterviewBit Amazon Problems on the Best Time to Buy and Sell Stocks.
+ */
 public class BuyingStocks {
 
     /**
      * Given an input stock price array, return the maximum profit that could be made if you optimally purchased and
      * sold stocks given those prices, given that you can only choose to buy, sell, or do nothing each day and
-     * you may only one share at a time.
+     * you may only own one share at a time.<br>
+     * <br>
+     * Solves the problem posed
+     * <a href="https://www.interviewbit.com/problems/best-time-to-buy-and-sell-stocks-ii/">here</a>
      *
      * @param stockPrices - an int array representing stock prices
      * @return The maximum profit that could be made with the constraints above
