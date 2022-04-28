0933b26e82261d4d2f7438177cdfea5c250d5b83
Kevin Mawhorter <kdmawhorter@gmail.com>
2022-04-28 12:55:39 -0400

    MIN_SUM_PATH_MATRIX: Added min sum functionality

--- src/main/java/interviewbit/MinSumPathMatrix.java
+++ src/main/java/interviewbit/MinSumPathMatrix.java
@@ -1,11 +1,27 @@
 package interviewbit;
 
 
+
+import lombok.Getter;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * A class designed to answer the problem related to InterviewBit Amazon Problems
  * <a href="https://www.interviewbit.com/problems/min-sum-path-in-matrix/">Min Sum Path in Matrix</a>.
  */
 public class MinSumPathMatrix {
+
+    private static class Pair {
+        @Getter
+        private final int m, n;
+
+        Pair(int m, int n) {
+            this.m = m;
+            this.n = n;
+        }
+    }
 
     /**
      * Returns the smallest path cost to reach (n, m) from (0,0) in NxM matrix inputMatrix.
@@ -13,6 +29,41 @@ public class MinSumPathMatrix {
      * @return The minimum sum path cost.
      */
     public int minSumPathInMatrix(int[][] inputMatrix) {
-        return 0;
+        int m = inputMatrix.length, n = inputMatrix[0].length;
+
+        int[][] remainingChildren = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                remainingChildren[i][j] += ((i<m-1) ? 1 : 0) + ((j<n-1) ? 1 : 0);
+            }
+        }
+        Queue<Pair> matrixElementQueue = new LinkedList<>();
+
+        matrixElementQueue.add(new Pair(m-1, n-1));
+
+        while (!matrixElementQueue.isEmpty()) {
+            Pair thisElem = matrixElementQueue.poll();
+            if  ((thisElem.getM()<m-1) && (thisElem.getN()<n-1)) {
+                inputMatrix[thisElem.getM()][thisElem.getN()] +=
+                        Math.min(inputMatrix[thisElem.getM()+1][thisElem.getN()],
+                                 inputMatrix[thisElem.getM()][thisElem.getN()+1]);
+            } else if (thisElem.getM()<m-1) {
+                inputMatrix[thisElem.getM()][thisElem.getN()] += inputMatrix[thisElem.getM()+1][thisElem.getN()];
+            } else if (thisElem.getN()<n-1) {
+                inputMatrix[thisElem.getM()][thisElem.getN()] += inputMatrix[thisElem.getM()][thisElem.getN()+1];
+            }
+            if (thisElem.getM()>0) {
+                if (--remainingChildren[thisElem.getM()-1][thisElem.getN()] == 0) {
+                    matrixElementQueue.add(new Pair(thisElem.getM()-1, thisElem.getN()));
+                }
+            }
+            if (thisElem.getN()>0) {
+                if (--remainingChildren[thisElem.getM()][thisElem.getN()-1] == 0) {
+                    matrixElementQueue.add(new Pair(thisElem.getM(), thisElem.getN()-1));
+                }
+            }
+        }
+        return inputMatrix[0][0];
     }
 }
