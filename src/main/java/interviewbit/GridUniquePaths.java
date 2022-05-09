cc6c105244501abf84f234dfbbedd08acb9bcf8c
Kevin Mawhorter <kdmawhorter@gmail.com>
2022-05-09 13:46:02 -0400

    GRID_UNIQ_PATH: Readme, tests, declarations, method

---
+++ src/main/java/interviewbit/GridUniquePaths.java
@@ -0,0 +1,19 @@
+package interviewbit;
+
+/**
+ * A class designed to answer the problem related to InterviewBit Adobe Problems
+ * <a href="https://www.interviewbit.com/problems/grid-unique-paths/">Grid Unique Paths</a>.
+ */
+public class GridUniquePaths {
+
+    /**
+     * Given an mxn grid, returns the total number of paths from (0,0) to (m-1, n-1) if allowed movement at cell (i,j)
+     * is to move to (i+1, j) or (i, j+1).
+     * @param m The height of the grid.
+     * @param n The width of the grid.
+     * @return The total number of paths through the grid.
+     */
+    public int countUniquePaths(int m, int n) {
+        return 0;
+    }
+}
---
+++ src/test/java/interviewbit/GridUniquePathsTest.java
@@ -0,0 +1,25 @@
+package interviewbit;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GridUniquePathsTest {
+
+    private GridUniquePaths gridUniquePaths;
+
+    @BeforeEach
+    void init() {
+        gridUniquePaths = new GridUniquePaths();
+    }
+
+    @Test
+    void countUniquePathsTest() {
+        assertEquals(1, gridUniquePaths.countUniquePaths(1, 1), "Test 1: 1x1");
+        assertEquals(1, gridUniquePaths.countUniquePaths(1, 2), "Test 1: 1x2");
+        assertEquals(1, gridUniquePaths.countUniquePaths(2, 1), "Test 3: 2x1");
+        assertEquals(2, gridUniquePaths.countUniquePaths(2, 1), "Test 4: 2x2");
+        assertEquals(6, gridUniquePaths.countUniquePaths(2, 1), "Test 5: 3x3");
+    }
+}
