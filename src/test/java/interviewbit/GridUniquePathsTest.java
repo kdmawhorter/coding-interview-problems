a12ab3d58a50a3b242ae27d65eb52dd89721dab5
Kevin Mawhorter <kdmawhorter@gmail.com>
2022-05-09 14:03:27 -0400

    GRID_UNIQ_PATH: Implemented method.

--- src/test/java/interviewbit/GridUniquePathsTest.java
+++ src/test/java/interviewbit/GridUniquePathsTest.java
@@ -19,7 +19,9 @@ public class GridUniquePathsTest {
         assertEquals(1, gridUniquePaths.countUniquePaths(1, 1), "Test 1: 1x1");
         assertEquals(1, gridUniquePaths.countUniquePaths(1, 2), "Test 1: 1x2");
         assertEquals(1, gridUniquePaths.countUniquePaths(2, 1), "Test 3: 2x1");
-        assertEquals(2, gridUniquePaths.countUniquePaths(2, 1), "Test 4: 2x2");
-        assertEquals(6, gridUniquePaths.countUniquePaths(2, 1), "Test 5: 3x3");
+        assertEquals(2, gridUniquePaths.countUniquePaths(2, 2), "Test 4: 2x2");
+        assertEquals(6, gridUniquePaths.countUniquePaths(3, 3), "Test 5: 3x3");
+        assertEquals(0, gridUniquePaths.countUniquePaths(0, 10), "Test 6: 0xN");
+        assertEquals(0, gridUniquePaths.countUniquePaths(10, 0), "Test 7: Mx0");
     }
 }
